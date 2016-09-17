fcb8ec2bba12bb78db1b02a22958ee4bc1469297
Archana Bajaj <bajaj@bu.edu>
2016-09-16 21:23:08 -0700

    Scans directory for python files
    Reports the files with missing doc string.
    Can be saved in different types of directories and checked for similar output files.

---
+++ undocumentedFiles.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+"""
+Created on Fri Sep 16 23:51:24 2016
+
+@author: Archana Bajaj
+"""
+
+#!/usr/bin/env python
+
+# Python 2/3 compatibility
+'''
+Scans current directory for *.py files and reports
+ones with missing __doc__ string.
+'''
+
+import sys
+PY3 = sys.version_info[0] == 3
+
+from glob import glob
+
+if __name__ == '__main__':
+    print('--- undocumented files:')
+    for fn in glob('*.py'):
+        loc = {}
+        try:
+            if PY3:
+                exec(open(fn).read(), loc)
+            else:
+                execfile(fn, loc)
+        except:
+            pass
+        if '__doc__' not in loc:
+            print(fn)
